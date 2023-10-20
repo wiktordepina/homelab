1b4b15e5a6d272784ffd4535f4ed69cb3426fbc0
Wiktor de Pina <wiktor.depina@simplybusiness.co.uk>
2023-10-20 18:55:28 +0100

    add ansible

---
+++ runner-toolbox/functions/ansible.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+export ANSIBLE_CONFIG='/build/ansible/.ansible.cfg'
+
+run_ansible_lxc() {
+  local vmid="${1}" ; check_null vmid "${1}"
+
+  local lxc_ip
+  lxc_ip=$(lxc_config "${vmid}" ".terraform.ip_address")
+  lxc_ip="${lxc_ip%/*}"
+
+  render_lxc_playbook "${vmid}" > playbook.yaml
+  trap 'rm -rf /build/playbook.yaml' EXIT
+
+  ansible-playbook -i "${lxc_ip}," playbook.yaml
+}
