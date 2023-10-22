a7f329ba2ba7bbe1ff3caf7ddab38a2b0f365d71
Wiktor de Pina <wiktor.depina@simplybusiness.co.uk>
2023-10-22 23:06:17 +0100

    updates

--- terraform/dns/versions.tf
+++ terraform/dns/versions.tf
@@ -11,7 +11,7 @@ terraform {
 provider "dns" {
   update {
     server        = var.dns_ip
-    key_name      = "tsig-key."
+    key_name      = "tsig-key"
     key_algorithm = "hmac-sha256"
     key_secret    = var.tsig_key
   }
