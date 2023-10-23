c8e1b3bcd1155bf834b50be258a6f4fb2d450fdd
Wiktor de Pina <wiktor.depina@simplybusiness.co.uk>
2023-10-23 18:39:10 +0100

    dns update

--- terraform/dns/main.tf
+++ terraform/dns/main.tf
@@ -1,5 +1,5 @@
 resource "dns_a_record_set" "router" {
-  zone = "home.matagoth.com."
+  zone = "matagoth.com."
   name = "router"
   addresses = [
     "10.10.0.1",
@@ -11,14 +11,14 @@ resource "dns_a_record_set" "router" {
 }
 
 resource "dns_a_record_set" "switch" {
-  zone      = "home.matagoth.com."
+  zone      = "matagoth.com."
   name      = "switch"
   addresses = ["192.168.200.50"]
   ttl       = 500
 }
 
 resource "dns_a_record_set" "accesspoint" {
-  zone      = "home.matagoth.com."
+  zone      = "matagoth.com."
   name      = "accesspoint"
   addresses = ["192.168.200.60"]
   ttl       = 500
