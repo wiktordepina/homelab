873ff037cc3e1e4f10a57c7b5ea7d0df6058af1f
Wiktor de Pina <wiktor.depina@simplybusiness.co.uk>
2023-11-01 19:23:50 +0000

    gh actions ntfy

---
+++ runner-toolbox/functions/ntfy.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+export NTFY_HOST='http://ntfy.home.matagoth.com'
+
+ntfy_msg() {
+  local topic="${1}"   ; check_null topic "${1}"
+  local title="${2}"   ; check_null title "${2}"
+  local message="${3}" ; check_null message "${3}"
+  local tags="${4}"
+  local extra_header="${5}"
+
+  local headers="-H 'Title: ${title}'"
+  [[ -n "${tags}" ]] && headers+=" -H 'Tags: ${tags}'"
+  [[ -n "${extra_header}" ]] && headers+=" -H '${extra_header}'"
+
+  eval "curl -u ${NTFY_CREDS} ${headers} -d '${message}' ${NTFY_HOST}/${topic}"
+}
+
+run_ntfy_workflow_status() {
+  local wf_name="${1}"   ; check_null wf_name "${1}"
+  local wf_status="${2}" ; check_null wf_status "${2}"
+  local wf_url="${3}"    ; check_null wf_url "${3}"
+
+  local status_tag='x'
+  [[ "${wf_status}" == 'success' ]] && status_tag='white_check_mark'
+
+  local extra_header="Actions: view, Show workflow, ${wf_url}, clear=true"
+
+  ntfy_msg 'gh-worker' "${wf_name}" "Workflow ${wf_name} completed with status ${wf_status}" "${status_tag}" "${extra_header}"
+}
