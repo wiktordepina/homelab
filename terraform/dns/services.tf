9740fe5ba73662085ba54b900f3bc7e0bbaba75f
Wiktor de Pina <wiktor.depina@simplybusiness.co.uk>
2023-11-26 16:32:46 +0000

    dns for jellyfin

--- terraform/dns/services.tf
+++ terraform/dns/services.tf
@@ -4,3 +4,10 @@ resource "dns_a_record_set" "ntfy" {
   addresses = ["10.20.1.202"]
   ttl       = 500
 }
+
+resource "dns_a_record_set" "jellyfin" {
+  zone      = "home.matagoth.com."
+  name      = "jellyfin"
+  addresses = ["10.20.1.204"]
+  ttl       = 500
+}
