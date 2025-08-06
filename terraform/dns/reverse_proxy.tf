967bde7f721be9e72b2e709062ec9972b0bb48bd
Wiktor de Pina <wiktordepina@gmail.com>
2025-08-06 21:09:21 +0100

    expand reverse proxy routes

--- terraform/dns/reverse_proxy.tf
+++ terraform/dns/reverse_proxy.tf
@@ -11,3 +11,17 @@ resource "dns_a_record_set" "rvp_proxmox" {
   addresses = ["10.20.1.10"]
   ttl       = 500
 }
+
+resource "dns_a_record_set" "rvp_prometheus" {
+  zone      = "homelab.matagoth.com."
+  name      = "prometheus"
+  addresses = ["10.20.1.10"]
+  ttl       = 500
+}
+
+resource "dns_a_record_set" "rvp_graphana" {
+  zone      = "homelab.matagoth.com."
+  name      = "graphana"
+  addresses = ["10.20.1.10"]
+  ttl       = 500
+}
