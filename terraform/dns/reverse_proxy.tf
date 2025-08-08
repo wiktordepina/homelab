8cb1dcd5642497556dc69e99deb6df78bf8b7f49
matagoth <matagoth@gmail.com>
2025-08-08 22:22:06 +0100

    reverse proxy update

--- terraform/dns/reverse_proxy.tf
+++ terraform/dns/reverse_proxy.tf
@@ -19,9 +19,58 @@ resource "dns_a_record_set" "rvp_prometheus" {
   ttl       = 500
 }
 
-resource "dns_a_record_set" "rvp_graphana" {
+resource "dns_a_record_set" "rvp_grafana" {
   zone      = "homelab.matagoth.com."
-  name      = "graphana"
+  name      = "grafana"
   addresses = ["10.20.1.10"]
+  ttl       = 500
+}
+
+resource "dns_a_record_set" "rvp_opnsense" {
+  zone      = "homelab.matagoth.com."
+  name      = "opnsense"
+  addresses = ["10.20.1.10"]
+  ttl       = 500
+}
+
+resource "dns_a_record_set" "rvp_uptime_kuma" {
+  zone      = "homelab.matagoth.com."
+  name      = "uptime-kuma"
+  addresses = ["10.20.1.10"]
+  ttl       = 500
+}
+
+resource "dns_a_record_set" "rvp_switch" {
+  zone      = "homelab.matagoth.com."
+  name      = "switch"
+  addresses = ["192.168.200.50"]
+  ttl       = 500
+}
+
+resource "dns_a_record_set" "rvp_pihole" {
+  zone      = "homelab.matagoth.com."
+  name      = "pihole"
+  addresses = ["192.168.200.50"]
+  ttl       = 500
+}
+
+resource "dns_a_record_set" "rvp_portainer" {
+  zone      = "homelab.matagoth.com."
+  name      = "portainer"
+  addresses = ["192.168.200.50"]
+  ttl       = 500
+}
+
+resource "dns_a_record_set" "rvp_prowlarr" {
+  zone      = "homelab.matagoth.com."
+  name      = "prowlarr"
+  addresses = ["192.168.200.50"]
+  ttl       = 500
+}
+
+resource "dns_a_record_set" "rvp_jellyfin" {
+  zone      = "homelab.matagoth.com."
+  name      = "jellyfin"
+  addresses = ["192.168.200.50"]
   ttl       = 500
 }
