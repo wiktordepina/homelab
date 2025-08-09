630fc009e0c7b4ae18ec5da38440ab655b2da4b1
Wiktor de Pina <wiktordepina@gmail.com>
2025-08-09 12:58:46 +0100

    dry by templating

--- terraform/dns/reverse_proxy.tf
+++ terraform/dns/reverse_proxy.tf
@@ -1,76 +1,28 @@
-resource "dns_a_record_set" "www" {
+locals {
+  reverse_proxy_servers = [
+    "www",
+    "proxmox",
+    "prometheus",
+    "grafana",
+    "opnsense",
+    "uptime-kuma",
+    "switch",
+    "pihole",
+    "portainer",
+    "prowlarr",
+    "jellyfin",
+    "qbittorrent",
+    "sabnzbd",
+    "fileserver",
+    "localai"
+  ]
+}
+
+resource "dns_a_record_set" "rvp" {
+  for_each = toset(local.reverse_proxy_servers)
+
   zone      = "homelab.matagoth.com."
-  name      = "www"
+  name      = each.key
   addresses = ["10.20.1.10"]
-  ttl       = 500
-}
-
-resource "dns_a_record_set" "rvp_proxmox" {
-  zone      = "homelab.matagoth.com."
-  name      = "proxmox"
-  addresses = ["10.20.1.10"]
-  ttl       = 500
-}
-
-resource "dns_a_record_set" "rvp_prometheus" {
-  zone      = "homelab.matagoth.com."
-  name      = "prometheus"
-  addresses = ["10.20.1.10"]
-  ttl       = 500
-}
-
-resource "dns_a_record_set" "rvp_grafana" {
-  zone      = "homelab.matagoth.com."
-  name      = "grafana"
-  addresses = ["10.20.1.10"]
-  ttl       = 500
-}
-
-resource "dns_a_record_set" "rvp_opnsense" {
-  zone      = "homelab.matagoth.com."
-  name      = "opnsense"
-  addresses = ["10.20.1.10"]
-  ttl       = 500
-}
-
-resource "dns_a_record_set" "rvp_uptime_kuma" {
-  zone      = "homelab.matagoth.com."
-  name      = "uptime-kuma"
-  addresses = ["10.20.1.10"]
-  ttl       = 500
-}
-
-resource "dns_a_record_set" "rvp_switch" {
-  zone      = "homelab.matagoth.com."
-  name      = "switch"
-  addresses = ["192.168.200.50"]
-  ttl       = 500
-}
-
-resource "dns_a_record_set" "rvp_pihole" {
-  zone      = "homelab.matagoth.com."
-  name      = "pihole"
-  addresses = ["192.168.200.50"]
-  ttl       = 500
-}
-
-resource "dns_a_record_set" "rvp_portainer" {
-  zone      = "homelab.matagoth.com."
-  name      = "portainer"
-  addresses = ["192.168.200.50"]
-  ttl       = 500
-}
-
-resource "dns_a_record_set" "rvp_prowlarr" {
-  zone      = "homelab.matagoth.com."
-  name      = "prowlarr"
-  addresses = ["192.168.200.50"]
-  ttl       = 500
-}
-
-resource "dns_a_record_set" "rvp_jellyfin" {
-  zone      = "homelab.matagoth.com."
-  name      = "jellyfin"
-  addresses = ["192.168.200.50"]
   ttl       = 500
 }
