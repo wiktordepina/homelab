98f7dd237e405a4550f08fe298c5428a9cd0b347
matagoth <matagoth@gmail.com>
2025-08-27 19:05:47 +0100

    update n8n

--- terraform/dns/services.tf
+++ terraform/dns/services.tf
@@ -19,9 +19,16 @@ resource "dns_a_record_set" "localai" {
   ttl       = 500
 }
 
- resource "dns_a_record_set" "prometheus" {
+resource "dns_a_record_set" "prometheus" {
   zone      = "home.matagoth.com."
   name      = "prometheus"
   addresses = ["10.20.1.210"]
+  ttl       = 500
+}
+
+resource "dns_a_record_set" "n8n" {
+  zone      = "home.matagoth.com."
+  name      = "n8n"
+  addresses = ["10.20.1.211"]
   ttl       = 500
 }
